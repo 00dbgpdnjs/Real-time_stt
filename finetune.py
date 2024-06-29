59ee2ad10650171aa1bc18f18538b0fc3163b45d
00dbgpdnjs <00dbgpdnjs@naver.com>
2024-06-30 02:53:11 +0900

    add load_dataset() and register it to argparse

--- finetune.py
+++ finetune.py
@@ -10,15 +10,54 @@ from datasets import load_dataset, DatasetDict
 
 def get_config() -> argparse.ArgumentParser:
     '''Wisper finetuning args parsing function'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--train-set', '-t',
+        required=True,
+        help='Train dataset name (file name or file path)'
+    )
+    parser.add_argument(
+        '--valid-set', '-v',
+        required=True,
+        help='Validation dataset name (file name or file path)'
+    )
+    parser.add_argument(
+        '--test-set', '-e',
+        required=True,
+        help='Test dataset name (file name or file path)'
+    )
+    config = parser.parse_args()
+    return config
     
 class Trainer:
     '''Whisper finetune trainer'''
     
-    def __init__(self) -> None:
+    def __init__(self, config) -> None:
         '''Init all required args for whisper finetune'''
+        self.config = config
     
     def load_dataset(self,) -> DatasetDict:
         '''Build dataset containing train/valid/test sets'''
+        dataset = DatasetDict()
+        dataset['train'] = load_dataset(
+            path='csv',
+            name='aihub-ko',
+            split='train',
+            data_files=self.config.train_set
+        )
+        dataset['valid'] = load_dataset(
+            path='csv',
+            name='aihub-ko',
+            split='train', # valid로 바꾸면 안됨
+            data_files=self.config.valid_set
+        )
+        dataset['test'] = load_dataset(
+            path='csv',
+            name='aihub-ko',
+            split='train',
+            data_files=self.config.test_set
+        )
+        return dataset
     
     def compute_metrics(self, pred) -> dict:
         '''Prepare evaluation metric (wer, cer, etc.)'''
@@ -42,4 +81,5 @@ class Trainer:
 if __name__ == '__main__':
     config = get_config()
     trainer = Trainer(config)
-    trainer.run()
+    result = trainer.load_dataset()
+    print(result)
